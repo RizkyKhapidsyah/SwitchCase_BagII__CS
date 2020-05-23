4879852f43515e4c7e418c9e31f72c986eba178a
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-24 00:29:49 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwitchCase_BagII__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Password Yang Anda Masukkan Salah, Coba Lagi");
+            Console.Write("Masukkan Password  : ");
+
+            String pass = Console.ReadLine();
+
+            switch (pass)
+            {
+                case "Password":
+                    Console.Clear();
+                    Console.WriteLine("Selamat datang di ruang administrator ");
+                    Console.WriteLine("======================================");
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Anda tidak berhak masuk kedalam sistem ini");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
+
